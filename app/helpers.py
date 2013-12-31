bbcf448386f076a62491d723386bebf1fbafcda2
Jesse Patching <jesse@patching.ca>
2013-12-31 11:17:51 -0700

    Detect if auth header present

--- app/helpers.py
+++ app/helpers.py
@@ -2,4 +2,7 @@ from app import app
 from flask import request
 
 def is_authenticated():
+    if 'Authorization' not in request.headers:
+        return False
+
     return app.config['API_KEY'] == request.headers['Authorization'].strip()
