9fa0fc35b6532b074f2e73c49339d327822cfe91
Jesse Patching <jesse@patching.ca>
2015-07-05 14:32:58 -0600

    Support mysql2:// urls

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     API_KEY = getenv('API_KEY')
-    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL').replace('mysql2:', 'mysql:')
     MAILGUN_API_URL = "https://api.mailgun.net/v2"
     MAILGUN_API_KEY = getenv('MAILGUN_API_KEY')
 
